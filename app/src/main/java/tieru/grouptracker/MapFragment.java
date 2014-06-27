9a7b9bebd332ea8be1b4fcacbe85ddbc8eaf216d
Tieru <sl.tieru@gmail.com>
2014-06-28 00:32:26 +0800

    Project commit

---
+++ app/src/main/java/tieru/grouptracker/MapFragment.java
@@ -0,0 +1,19 @@
+package tieru.grouptracker;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by tieru on 6/28/14.
+ */
+public class MapFragment extends Fragment{
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.activity_map, container, false);
+        return view;
+    }
+}
